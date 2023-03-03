4d6e360917ebf49fdb5c48a6f33d0fed48bc4b58
Kacper Szlajter <szlajter.kacper@outlook.com>
2023-03-03 16:09:50 +0100

    initial commit

---
+++ API/Entities/User.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Entities
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+        public string UserName { get; set; }
+    }
+}
