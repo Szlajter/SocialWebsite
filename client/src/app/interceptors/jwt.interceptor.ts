5c7203bb204b0724b2f70d4b494add65d8f07847
Kacper Szlajter <szlajter.kacper@outlook.com>
2024-04-12 22:47:58 +0200

    Add frontend auto logout after token expires

--- client/src/app/interceptors/jwt.interceptor.ts
+++ client/src/app/interceptors/jwt.interceptor.ts
@@ -7,16 +7,21 @@ import {
 } from '@angular/common/http';
 import { Observable, take } from 'rxjs';
 import { AccountService } from '../services/account.service';
+import { ToastrService } from 'ngx-toastr';
 
 @Injectable()
 export class JwtInterceptor implements HttpInterceptor {
 
-  constructor(private accountService: AccountService) {}
+  constructor(private accountService: AccountService, private toastr: ToastrService) {}
   //adds jwt token to logged user requests
   intercept(request: HttpRequest<unknown>, next: HttpHandler): Observable<HttpEvent<unknown>> {
     this.accountService.currentUser$.pipe(take(1)).subscribe({
       next: user => {
-        if(user){
+        if(user && user.token){
+          if (this.accountService.isTokenExpired(user.token)) {
+            this.accountService.logout();
+            this.toastr.error("Token expired. User logged out");
+          }
           request = request.clone({
             setHeaders: {
               Authorization: `Bearer ${user.token}`
--- client/src/app/services/account.service.ts
+++ client/src/app/services/account.service.ts
@@ -4,6 +4,7 @@ import { BehaviorSubject, map } from 'rxjs';
 import { User } from '../models/user';
 import { environment } from 'src/environments/environment.development';
 import { StatusService } from './status.service';
+import { Router } from '@angular/router';
 
 @Injectable({
   providedIn: 'root'
@@ -13,7 +14,7 @@ export class AccountService {
   private currentUserSource = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient, private statusService: StatusService) { }
+  constructor(private http: HttpClient, private statusService: StatusService, private router: Router) { }
   
   login(model: any){
     return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
@@ -48,10 +49,23 @@ export class AccountService {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
     this.statusService.stopHubConnection();
+    window.location.reload();
   }
 
   //atob is marked as deprecated, but it has a second overload that remains functional.
   getDecodedToken(token: string) {
     return JSON.parse(atob(token.split('.')[1]))
+  }
+
+  isTokenExpired(token: string): boolean {
+    try {
+      const decodedToken = this.getDecodedToken(token);
+      const date = new Date(0);
+      date.setUTCSeconds(decodedToken.exp);
+      return date.valueOf() < new Date().valueOf();
+    }
+    catch (error) {
+      return false;
+    }
   }
 }
